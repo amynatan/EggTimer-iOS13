96dc50a4f76fac930f6a4167317c8541a807b275
amynata <amynata.nepomuceno@gmail.com>
2020-04-23 01:04:25 -0400

    adicionar progresso gradual da ProgressBar. Adicionar alarme ao fim do timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
     
@@ -17,11 +19,10 @@ class ViewController: UIViewController {
     // instead of declaring several constants, we can create a dictionary.
     //If using several constants, we could use either a switch or an if else statement
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
-    
-    var counter = 60
-    
+    var totalTime = 0
+    var secondsPassed = 0
     var timer = Timer() //this was only created so we can call the timer
-
+    var player: AVAudioPlayer!
     
     @IBOutlet weak var mainLabel: UILabel!
     
@@ -30,31 +31,40 @@ class ViewController: UIViewController {
     @IBAction func hardnessSelected(_ sender: UIButton) {
 
         
-        
         timer.invalidate() //this stops any timer that's running when the button is pressed
         
         //the hardness constant allows us to assign the current tytle of the button to other things
         let hardness = sender.currentTitle! //this is going to be either "Soft", "Hard" or "Medium"
-      
+        totalTime = eggTimes[hardness]!
+        
                         //here hardness will be replaced with the button title which is the same as the dictionary key
-        counter = eggTimes[hardness]! //dictionaries will always prouce optional int in case we misspell the key so it's necessary to confirm that there are no typos by unwrapping the result (!)
+        totalTime = eggTimes[hardness]! //dictionaries will always prouce optional int in case we misspell the key so it's necessary to confirm that there are no typos by unwrapping the result (!)
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        mainLabel.text = hardness
         
         //Timer.scheduledTimer would have worked without the timer variable. it's only there so we can trigger it and stop it
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
         
 }
-
     
 
-@objc func updateCounter() {
-    //example functionality
-    if counter > 0 {
-        print("\(counter) seconds")
-        counter -= 1
+@objc func updateTimer() {
+    
+    if secondsPassed < totalTime {
+        secondsPassed += 1
+        let percentageProgress = Float(secondsPassed) / Float(totalTime)
+        progressBar.progress = percentageProgress
+        
     } else {
         timer.invalidate()
-        mainLabel.text = "Done"
+        mainLabel.text = "Done!"
+        
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
         
     }
 }
