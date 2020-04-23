6c05dc1ccbf908f86057b780c42aa1cfd78c2e68
amynata <amynata.nepomuceno@gmail.com>
2020-04-22 23:07:59 -0400

    adicionar função de extrair o resultado do dicionário usando o botões como input

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+ /* let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12*/
+    
+    // instead of declaring several constants, we can create a dictionary.
+    //If using several constants, we could use either a switch or an if else statement
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle! //this is going to be either "Soft", "Hard" or "Medium"
+        
+        print(eggTimes[hardness]!) //dictionaries will always prouce optional int in case we misspell the key so it's necessary to confirm that there are no typos by unwrapping the result (!)
+      
+}
 }
